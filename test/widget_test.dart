49a33ae43cc09ab50fa51db08c2d6c8aced02375
UsamaFathi <117278673+UsamaFathi@users.noreply.github.com>
2026-02-23 00:46:35 +0200

    Continue flow with caregiver linking screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,12 +3,18 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pharmax/main.dart';
 
 void main() {
-  testWidgets('Pharmax home screen renders expected sections', (tester) async {
+  testWidgets('Caregiver linking screen renders expected elements', (tester) async {
     await tester.pumpWidget(const PharmaxApp());
 
-    expect(find.text('Pharmax'), findsOneWidget);
-    expect(find.text('Categories'), findsOneWidget);
-    expect(find.text('Popular Products'), findsOneWidget);
-    expect(find.text('Go to Cart'), findsOneWidget);
+    expect(find.text('Caregiver Linking'), findsOneWidget);
+    expect(find.text('Caregiver mode'), findsOneWidget);
+    expect(find.text('Link caregiver'), findsOneWidget);
+    expect(find.text('Caregiver phone'), findsOneWidget);
+    expect(find.text('Send invite'), findsOneWidget);
+    expect(find.text('Notifications to caregiver'), findsOneWidget);
+    expect(find.text('Missed dose'), findsOneWidget);
+    expect(find.text('Wrong time'), findsOneWidget);
+    expect(find.text('Refill needed'), findsOneWidget);
+    expect(find.text('High-risk interaction'), findsOneWidget);
   });
 }
