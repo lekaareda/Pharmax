0dad2d63302b5abe25bad92f8f93eda2d13e8b57
UsamaFathi <117278673+UsamaFathi@users.noreply.github.com>
2026-02-23 01:39:11 +0200

    Continue UI flow with insurance screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,12 +3,26 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:pharmax/main.dart';
 
 void main() {
-  testWidgets('Pharmax home screen renders expected sections', (tester) async {
+  testWidgets('Insurance screen renders expected elements', (tester) async {
     await tester.pumpWidget(const PharmaxApp());
 
-    expect(find.text('Pharmax'), findsOneWidget);
-    expect(find.text('Categories'), findsOneWidget);
-    expect(find.text('Popular Products'), findsOneWidget);
-    expect(find.text('Go to Cart'), findsOneWidget);
+    expect(find.text('Insurance'), findsOneWidget);
+    expect(find.text('lekaa reda'), findsOneWidget);
+
+    expect(find.text('Provider'), findsOneWidget);
+    expect(find.text('Axa'), findsOneWidget);
+    expect(find.text('Card #'), findsOneWidget);
+    expect(find.text('1234 5678 9012'), findsOneWidget);
+    expect(find.text('Coverage'), findsOneWidget);
+    expect(find.text('80%'), findsOneWidget);
+
+    expect(find.text('Use insurance'), findsOneWidget);
+    expect(find.text('Remaining to pay: \$20'), findsOneWidget);
+    expect(find.text('Verify'), findsOneWidget);
+
+    expect(find.text('Home'), findsOneWidget);
+    expect(find.text('Orders'), findsOneWidget);
+    expect(find.text('Chat'), findsOneWidget);
+    expect(find.text('Profile'), findsOneWidget);
   });
 }
